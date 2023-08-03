ec7464a0f9d8d29ec0c5b11b453b873e68bfab6b
hzdelgado <88523514+hzdelgado@users.noreply.github.com>
2023-08-03 15:42:09 -0500

    flask dep and others installed
    removed unnecesary imports

--- api/ApiHandler.py
+++ api/ApiHandler.py
@@ -1,9 +1,9 @@
 
-from flask_restful import Api, Resource, reqparse
+from flask_restful import Resource
 
 class ApiHandler(Resource):
   def get(self):
     return {
       'resultStatus': 'SUCCESS',
-      'message': "Hello Api Handler"
+      'message': "Successful call"
       }
